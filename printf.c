aeba6ef50b747ebd9edb90393bbf2d0a4613a017
hopemumbi <mumbimak98@gmail.com>
2024-05-01 07:29:40 +0300

    update .

--- printf.c
+++ printf.c
@@ -1,15 +1,55 @@
 #include <unistd.h>
+#include <stdarg.h>
+/**
+ * _printf - produces output according to a format.
+ *
+ * @format: A pointer to a format string
+ * ...: A list of arguments passed to format specifiers
+ *
+ * Return: : the number of characters printed (excluding the null byte
+ */
 int _printf(const char *format, ...)
 {
+	va_list args;
 	int i = 0;
 	size_t len = 0;
+	char *str, buffer[1024];
 
-	while (format[i] != '\0')
+	va_start(args, format);
+
+	while (format && format[i] != '\0')
 	{
-		i++;
-		len++;
-	}
+		if (format[i] == '%')
+		{
+			i++; /* Skip the '%' */
+			switch (format[i])
+			{
+				case 'c':
+					buffer[len++] = va_arg(args, int);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					while (*str != '\0')
+					{
+						buffer[len++] = *str++;
+					}
+					break;
+				default:
+					buffer[len++] = '%';
+					buffer[len++] = format[i];
+					break;
 
-		write(1, format, len);
+			}
+
+		}
+		else
+		{
+			buffer[len++] = format[i];
+		}
+		i++;
+	}
+	buffer[len] = '\0';
+	write(1, buffer, len);
+	va_end(args);
 	return (0);
 }
