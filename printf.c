8eb62286c0a75593ba6775fdbbca3bae0e9c4966
hopemumbi <mumbimak98@gmail.com>
2024-05-02 08:57:37 +0300

    update printf.c

--- printf.c
+++ printf.c
@@ -1,6 +1,9 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
+#include <stdlib.h>
+#define CAPACITY 1024
+
 /**
  * _printf - produces output according to a format.
  *
@@ -13,8 +16,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, j;
-	size_t len = 0;
-	char buffer[1024];
+	size_t len = 0, k;
+	char *buffer, *new_buffer;
 	format_specifier f[] = {
 		{"c", print_c},
 		{"s", print_s},
@@ -22,9 +25,32 @@ int _printf(const char *format, ...)
 		{"i", print_i}
 	};
 
+	if (format == NULL)
+		return (-1);
+
+	buffer = (char *)malloc(CAPACITY * sizeof(char));
+	if (buffer == NULL)
+		return (-1);
+
 	va_start(args, format);
 	while (format && format[i] != '\0')
 	{
+		if (len >= CAPACITY - 1)
+		{
+			new_buffer = (char *)malloc((CAPACITY * 2) * sizeof(char));
+			if (new_buffer == NULL)
+			{
+				free(buffer);
+				va_end(args);
+				return (-1);
+			}
+			for (k = 0; k < len; k++)
+			{
+				new_buffer[k] = buffer[k];
+			}
+			free(buffer);
+			buffer = new_buffer;
+		}
 		if (format[i] == '%')
 		{
 			i++; /* Skip the '%' */
@@ -57,6 +83,7 @@ int _printf(const char *format, ...)
 	}
 	buffer[len] = '\0';
 	write(1, buffer, len);
+	free(buffer);
 	va_end(args);
 	return (len);
 }
